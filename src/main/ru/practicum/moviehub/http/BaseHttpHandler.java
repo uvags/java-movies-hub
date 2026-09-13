c93790b6c39a43209f535bce612ae86d0c68f418
uvags <uvadev@yandex.ru>
2026-09-13 11:40:48 +0700

    fix: To delete empty try block

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -31,9 +31,7 @@ public abstract class BaseHttpHandler implements HttpHandler {
         // для отправки ответа без тела и кодом 204
         ex.getResponseHeaders().set("Content-Type", CT_JSON);
         ex.sendResponseHeaders(204, -1);
-
-        try (OutputStream os = ex.getResponseBody()) {
-        }
+        ex.close();
     }
 
     protected void sendError(HttpExchange ex, int status, String message) throws IOException {
