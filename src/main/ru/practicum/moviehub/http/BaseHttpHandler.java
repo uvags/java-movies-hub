b6bd2be7fd2d9b06645f54efe7869d9e867f1f13
uvags <uvadev@yandex.ru>
2026-09-14 15:29:56 +0700

    fix: To delete Content-Type in response where 204 and empty body

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -25,7 +25,6 @@ public abstract class BaseHttpHandler implements HttpHandler {
     }
 
     protected void sendNoContent(HttpExchange ex) throws IOException {
-        ex.getResponseHeaders().set("Content-Type", CT_JSON);
         ex.sendResponseHeaders(204, -1);
         ex.close();
     }
